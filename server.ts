191987d7d3f32c652bc6056238c5aa946ad8c6c1
JohnnyASan <johnanthonysanabria@gmail.com>
2024-11-11 22:09:02 -0700

    fix for express-session

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ const app = express();
 
 import session from 'express-session';
 import passport from 'passport';
+import { Strategy as GitHubStrategy } from 'passport-github2';
 
 import path from 'path';
 import dotenv from 'dotenv';
@@ -16,8 +17,7 @@ import NotFoundError from './src/errors/notFoundError';
 import MongoError from './src/errors/mongoError'; 
 import HttpStatus from 'http-status-codes';
 import routes from './src/routes';
-
-
+import User from './src/models/user';
 
 
 connectDB();
@@ -34,6 +34,48 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_OAUTH_CLIENT_ID ?? '',
+  clientSecret: process.env.GITHUB_OAUTH_SECRET ?? '',
+  callbackURL: process.env.GITHUB_CALLBACK_URL ?? ''
+  },
+  async (accessToken: string, refreshToken: string, profile: any, callback: any) => {
+      console.log("PROFILE_LOG: \n");
+      console.log(profile)
+      let user = await User.findOne({ githubId: profile.id });
+      if (!user) {
+          console.log('Creating new user from GitHub OAuth in DB...');
+          const newUser = new User({
+              username: profile._json.login,
+              profileImage: profile._json.avatar_url,
+              githubId: profile.id,
+              githubToken: accessToken,
+              createdAt: Date.now(),
+              updatedAt: Date.now()
+          });
+          await newUser.save();
+
+          return callback(null, profile);
+      }
+      else {
+          console.log('Github user already exists in DB...');
+          return callback(null, profile);
+      }
+  }
+));
+
+// Configure Passport authenticated session persistence 
+passport.serializeUser((user: any, done) => { done(null, user.id); }); 
+passport.deserializeUser(async (id: string, done) => {
+   try { 
+      const user = await User.findById(id);
+       done(null, user); 
+      } catch (err) {
+           done(err);
+      }
+  }
+);
 
 app.use(function handleValidationError(err: any, req: Request, res: Response, next: NextFunction) {
   if (err instanceof ValidationError) {
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,49 +1,20 @@
 import express, {Request, Response} from 'express';
-import User from '../models/user';
-import { Strategy as GitHubStrategy } from 'passport-github2';
+
 import passport from 'passport';
 import dotenv from 'dotenv';
 import eSeshD from '../types/express-session';
 dotenv.config();
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_OAUTH_CLIENT_ID ?? '',
-    clientSecret: process.env.GITHUB_OAUTH_SECRET ?? '',
-    callbackURL: process.env.GITHUB_CALLBACK_URL ?? ''
-    },
-    async (accessToken: string, refreshToken: string, profile: any, callback: any) => {
-        console.log("PROFILE_LOG: \n");
-        console.log(profile)
-        let user = await User.findOne({ githubId: profile.id });
-        if (!user) {
-            console.log('Creating new user from GitHub OAuth in DB...');
-            const newUser = new User({
-                username: profile._json.login,
-                profileImage: profile._json.avatar_url,
-                githubId: profile.id,
-                githubToken: accessToken,
-                createdAt: Date.now(),
-                updatedAt: Date.now()
-            });
-            await newUser.save();
 
-            return callback(null, profile);
-        }
-        else {
-            console.log('Github user already exists in DB...');
-            return callback(null, profile);
-        }
-    }
-));
 
 const authRouter = express.Router();
 
 authRouter.get('/', passport.authenticate('github', { scope: ['user:email'] }));
 authRouter.get(
     '/callback', 
-    passport.authenticate('github', { failureRedirect: '/auth/github/error'}),
+    passport.authenticate('github', { failureRedirect: '/'}),
     function (req, res) {
-        res.redirect('/auth/github/success');
+        res.redirect('/');
     }
 );
 authRouter.get('/success', async (req: Request, res: Response) => {
