5771d2aa8fb7383c2d8e556aa24c30e8d869dbbb
JohnnyASan <johnanthonysanabria@gmail.com>
2024-11-11 21:23:20 -0700

    added print of profile object

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -13,6 +13,7 @@ passport.use(new GitHubStrategy({
     callbackURL: process.env.GITHUB_CALLBACK_URL ?? ''
     },
     async (accessToken: string, refreshToken: string, profile: any, callback: any) => {
+        console.log(profile);
         let user = await User.findOne({ githubId: profile.id });
         if (!user) {
             console.log('Creating new user from GitHub OAuth in DB...');
